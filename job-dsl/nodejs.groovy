4f367d9483d19a562beabb5fa90eee323cd4dfdf
Oludemilade Raji <oludemiladeraji@gmail.com>
2019-01-18 17:58:59 -0800

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git@github.com:oludemilade/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/oludemilade/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('Oludemilade')
             node / gitConfigEmail('oludemiladeraji@gmail.com')
         }
